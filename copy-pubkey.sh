6422e419c564ad149aab9f3e8ee4733cacbc880c
bjkim <bjkim@dangolsports.com>
2014-01-20 11:38:09 +0900

    add .gemrc

---
+++ copy-pubkey.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xclip -selection c -i ~/.ssh/id_rsa.pub
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp .emacs ..
+cp .bashrc ..
+cp .xinitrc ..
+
+wget https://raw.github.com/keenerd/packer/master/packer
+
